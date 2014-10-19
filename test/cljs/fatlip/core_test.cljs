fdb8d5c2893acc692ad15f229e097a08c28778bd
Brent Hagany <brent.hagany@gmail.com>
2014-10-18 23:00:57 -0500

    Add test for replace-ps

--- test/cljs/fatlip/core_test.cljs
+++ test/cljs/fatlip/core_test.cljs
@@ -47,7 +47,6 @@
     ;; ESK ordering
     (is (= (count (:marked ordered-graph)) 1) "Number of marked edges")
     (is (= (:crossings ordered-graph) 5) "Number of crossings")
-
     (is (empty? (->> (:layers ordered-graph)
                      (mapcat #(:minus-ps %))
                      (filter #(contains? (:p ordered-graph) %))))
@@ -56,3 +55,21 @@
                      (mapcat #(:minus-qs %))
                      (filter #(contains? (:q ordered-graph) %))))
         "No q nodes in :minus-qs")))
+
+
+;; Fine-grained ESK
+(deftest replace-ps
+  (let [p (f/Node. :0-0 0 #{:a})
+        not-p (f/Node. :0-1 0 #{:b})
+        p-succ (f/Node. :1-0 0 #{:a})
+        not-p-succ (f/Node. :1-0 0 #{:b})
+        seg-c-edge (f/Edge. (f/Node. :-1:55 -1 #{:c}) (f/Node. :3-99 3 #{:c}) #{:c})
+        seg-c (f/SegmentContainer. [seg-c-edge])
+        p-edge (f/Edge. p p-succ #{:a})
+        graph {:p #{p}
+               :succs {p #{p-edge}, not-p #{(f/Edge. not-p not-p-succ #{:b})}}}
+        layer (-> (f/Layer. 0 0 [p seg-c not-p])
+                  (assoc :ordered [p seg-c not-p]))]
+    (is (= (f/replace-ps graph layer)
+           (assoc layer :minus-ps [(f/SegmentContainer. [p-edge seg-c-edge]) not-p]))
+        "P nodes get replaced by segment containers, and joined with adjacent segment containers")))
