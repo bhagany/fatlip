97f186c843aeeff027e150aa9b549e9ae719d628
Brent Hagany <brent.hagany@gmail.com>
2014-10-19 22:28:16 -0500

    Indulge naming pedantry

--- src/cljs/fatlip/core.cljs
+++ src/cljs/fatlip/core.cljs
@@ -38,12 +38,12 @@
     (map->Node (assoc input :id node-id :layer-id layer-id))))
 
 
-(defn- add-p-q-nodes [graph last-node node edge-characters]
+(defn- add-p-q-nodes [graph last-node node characters]
   "When an edge would span more than 2 layers, two helper nodes are created. P nodes
   are placed on the layer following last-node's layer, and q nodes are placed on the
   layer preceding node's layer. Edges are then drawn from last-node -> p node and from
   p node to q node, regardless of the span between the p node and the q node"
-  (let [proto-node {:characters edge-characters}
+  (let [proto-node {:characters characters}
         p-layer-id (inc (:layer-id last-node))
         q-layer-id (dec (:layer-id node))
         p-node (make-node graph p-layer-id proto-node)
@@ -53,33 +53,33 @@
               (update-in [:layers q-layer-id :nodes] conj q-node)
               (update-in [:p] conj p-node)
               (update-in [:q] conj q-node)
-              (add-edge last-node p-node edge-characters)
-              (add-edge p-node q-node edge-characters))]
+              (add-edge last-node p-node characters)
+              (add-edge p-node q-node characters))]
     [g q-node]))
 
 
-(defn- add-r-node [graph last-node node edge-characters]
+(defn- add-r-node [graph last-node node characters]
   "R nodes are created to assist in drawing the graph, when an edge skips over a layer.
   In that case, the r node is placed in the intervening layer, and an edge is created
   from last-node to r node"
   (let [r-layer-id (inc (:layer-id last-node ))
-        r-node (make-node graph r-layer-id {:characters edge-characters})
+        r-node (make-node graph r-layer-id {:characters characters})
         g (-> graph
               (update-in [:layers r-layer-id :nodes] conj r-node)
               (update-in [:r] conj r-node)
-              (add-edge last-node r-node edge-characters))]
+              (add-edge last-node r-node characters))]
     [g r-node]))
 
 
-(defn- process-edge-characters [graph last-node node edge-characters]
+(defn- process-edge-characters [graph last-node node characters]
   "Create edges and possibly new helper nodes for a set of characters that
   remain together from last-node to node"
   (let [span (- (:layer-id node) (:layer-id last-node))
         [g last-n] (cond
-                    (= span 2) (add-r-node graph last-node node edge-characters)
-                    (> span 2) (add-p-q-nodes graph last-node node edge-characters)
+                    (= span 2) (add-r-node graph last-node node characters)
+                    (> span 2) (add-p-q-nodes graph last-node node characters)
                     :else [graph last-node])]
-    (add-edge g last-n node edge-characters)))
+    (add-edge g last-n node characters)))
 
 
 (defn- process-characters
